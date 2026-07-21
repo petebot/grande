e09ca8ded733e108a3564a882850aba63d6788c6
Pete Nawara <pete@petenawara.com>
2026-07-21 16:49:22 -0400

    feat(studio): scaffold Sanity workspace

---
+++ apps/studio/sanity.config.ts
@@ -0,0 +1,16 @@
+import {visionTool} from '@sanity/vision'
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+
+import {schemaTypes} from './schemaTypes'
+
+export default defineConfig({
+  name: 'default',
+  title: 'Grande Burrito',
+  projectId: 'w6vleqf7',
+  dataset: 'development',
+  plugins: [structureTool(), visionTool()],
+  schema: {
+    types: schemaTypes,
+  },
+})
